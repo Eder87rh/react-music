8c61806150afc11c389015264e7dcb7716464dfd
Eder Ramirez <eder.ramirez87@gmail.com>
2019-12-26 09:13:15 -0600

    Consultando la API de letras

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
 
 function App() {
 
@@ -9,8 +10,15 @@ function App() {
   const [info, agregarInfo] = useState({});
 
   // Método para consultar la API de las letras de canciones
-  const consultarAPILetra = (busqueda) => {
-    console.log("desde el app.js", busqueda)
+  const consultarAPILetra = async (busqueda) => {
+    const {artista, cancion} = busqueda;
+    const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+    // consultar la api
+    const resultado = await axios(url);
+
+    // almacenar la letra en el state
+    agregarLetra(resultado.data.lyrics);
   }
 
   return (
